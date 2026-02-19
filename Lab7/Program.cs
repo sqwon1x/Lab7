6ac3a9f78cca7869418edf8cd64e9ef7438c3b61
Mykhailo Hutych <miichail.gutych@gmail.com>
2026-02-19 23:21:56 +0200

    Add project files.

---
+++ Lab7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Головна програма
+class Program
+{
+    static void Main()
+    {
+        List<Vehicle> vehicles = new List<Vehicle>
+        {
+            new Car("BMW", 120),
+            new Bicycle("Trek", 25),
+            new Airplane("Boeing", 800)
+        };
+
+        foreach (Vehicle v in vehicles)
+        {
+            v.Move();
+
+            if (v is IRefuelable refuelable)
+            {
+                refuelable.Refill();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
